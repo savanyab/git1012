b338ddc96d57b3219a324088a1b0baa06c79abb4
savanyab <savanyab@gmail.com>
2018-10-12 16:08:28 +0200

    brigi1.1

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 //v0.0.1
 const start = require('./index');
 const end = require('./console');
+
+const BrigiPullThatSwitch = () => {
+    console.log(end.kiiratas(start.name1, start.name2));
+};
+
+BrigiPullThatSwitch();

1456f322af45738d62d1dcf5680b8e4cbf8ce059
savanyab <savanyab@gmail.com>
2018-10-12 15:03:36 +0200

    savanyab/jav

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
-  name1: 'egy', value: 1,
-  name2: 'ketto', value: 2,
-  name3: 'harom', value: 3,
-  name4: 'negy', value: 4,
-  name5: 'ot', value: 5,
-  name6: 'hat', value: 6,
-  name7: 'het', value: 7,
-  name8: 'nyolc', value: 8,
-  name9: 'kilenc', value : 9,
-  name10: 'tiz', value: 10
+  name1: 'egy', value1: 1,
+  name2: 'ketto', value2: 2,
+  name3: 'harom', value3: 3,
+  name4: 'negy', value4: 4,
+  name5: 'ot', value5: 5,
+  name6: 'hat', value6: 6,
+  name7: 'het', value7: 7,
+  name8: 'nyolc', value8: 8,
+  name9: 'kilenc', value9 : 9,
+  name10: 'tiz', value10: 10
 };

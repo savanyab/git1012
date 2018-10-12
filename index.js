5e448fd93a7fa19d6454f6521de614495b9149ee
savanyab <savanyab@gmail.com>
2018-10-12 14:43:54 +0200

    savanyab/card3

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
-  key1: 'egy',
-  key2: 'ketto',
-  key3: 'harom',
-  key4: 'negy',
-  key5: 'ot',
-  key6: 'hat',
-  key7: 'het',
-  key8: 'nyolc',
-  key9: 'kilenc',
-  key10: 'tiz'
+  name1: 'egy', value1: 1,
+  name2: 'ketto', value2: 2,
+  name3: 'harom', value3: 3,
+  name4: 'negy', value4: 4,
+  name5: 'ot', value5: 5,
+  name6: 'hat', value6: 6,
+  name7: 'het', value7: 7,
+  name8: 'nyolc', value8: 8,
+  name9: 'kilenc', value9: 9,
+  name10: 'tiz', value10: 10
 };
